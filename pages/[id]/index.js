968a162eaaa494cb238d6a79e344d61d82a819cc
Manoel <manoeltavares.jr@gmail.com>
2020-07-10 12:54:09 -0300

    Build

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -24,6 +24,7 @@ class Home extends Component{
           <title>{this.props.repositories.infoCar.Modelo}</title>
           <meta property="og:image" content={this.props.repositories.image_anuncio[0].foto}/> 
           <meta property="og:description" content={`${this.props.repositories.infoCar.Marca} - ${this.props.repositories.infoCar.Modelo} - ${this.props.repositories.valor}` }/>
+          <meta property="og:site_name" content="VendeTudo.com"/>
         </Head>
       </>
     );
